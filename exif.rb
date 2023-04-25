6bdc291a1c1c34f3a0e240a45b73419a7b1183da
kostek <ka.koszewski@gmail.com>
2023-04-26 01:37:58 +0200

    refactoring

--- exif.rb
+++ exif.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'exif'
 require 'json'
 require 'uri'
@@ -9,14 +11,14 @@ require './exif_helper'
 if ARGV.empty?
   p 'No arguments...'
   exit
-end 
+end
 
 if ARGV[0] =~ URI::ABS_URI
   ExifHelper::fetch_image(ARGV[0])
   image = Dir["#{ExifHelper::download_path}/*"].first
 else
-  image =  ARGV.first
-end 
+  image = ARGV.first
+end
 
 begin
   data = Exif::Data.new(IO.read(image))
@@ -30,19 +32,11 @@ end
 
 result = ExifHelper::flatten_hash data.to_h
 
-# result.delete(:maker_note)
-
 if ARGV[1, ARGV.size].include?('--json')
-  result[:maker_note] = result[:maker_note].force_encoding("ISO-8859-1")
+  result[:maker_note] = result[:maker_note].force_encoding('ISO-8859-1')
   p result.to_json
 else
   ExifHelper::print_result(result)
-end 
+end
 
 ExifHelper::delete_temp_image
-
-
-
-
-
-
--- exif_helper.rb
+++ exif_helper.rb
@@ -1,31 +1,33 @@
+# frozen_string_literal: true
+
 module ExifHelper
-  extend self
-  
-  def flatten_hash(param, prefix=nil)
+  module_function
+
+  def flatten_hash(param, _prefix = nil)
     param.each_pair.reduce({}) do |a, (k, v)|
-      v.is_a?(Hash) ? a.merge(flatten_hash(v, "#{k}.")) : a.merge("#{k}".to_sym => v)
+      v.is_a?(Hash) ? a.merge(flatten_hash(v, "#{k}.")) : a.merge(k.to_s.to_sym => v)
     end
   end
-  
+
   def print_result(params)
-    params.each { |k,v| p "#{k}: #{v}" }
-  end 
-  
+    params.each { |k, v| p "#{k}: #{v}" }
+  end
+
   def fetch_image(image_url)
-    self.delete_temp_image
+    delete_temp_image
 
     open(image_url) do |image|
-      File.open("#{self.download_path}/temp_image.jpg", "wb") do |file|
+      File.open("#{download_path}/temp_image.jpg", 'wb') do |file|
         file.write(image.read)
       end
     end
-  end 
+  end
 
   def download_path
     File.expand_path('temp', __dir__)
-  end 
+  end
 
   def delete_temp_image
-    FileUtils.rm_rf("#{self.download_path}/.", secure: true)
-  end 
-end 
+    FileUtils.rm_rf Dir.glob("#{download_path}/*.jpg")
+  end
+end
